75fe74c4405d445b6ddf9df766c009225163fd0f
plegrand2710 <pauline.legrand2710@gmail.com>
2024-11-11 20:29:35 -1000

    MAJ module commande - ajout récupération de la quantite
    ajout des références aux editTexts pour la quantité

--- DM/app/src/main/java/com/pauline/dm/FragmentPartage.java
+++ DM/app/src/main/java/com/pauline/dm/FragmentPartage.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 public class FragmentPartage extends Fragments {
     View view;
+    EditText etQ1 ;
+    EditText etQ2 ;
+    EditText etQ3 ;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +31,23 @@ public class FragmentPartage extends Fragments {
         autoCompletePlat.setThreshold(1);
         autoCompletePlat.setAdapter(adapterPlats);
 
+        etQ1 = (EditText) view.findViewById(R.id.platPartageQuantiteEditText);
+
         adapterAccompagnements = new ArrayAdapter<>(view.getContext(),
                 android.R.layout.simple_dropdown_item_1line, listeAccompagnements);
         AutoCompleteTextView autoCompleteAccompagnement = view.findViewById(R.id.accompagnementPartageEditText);
         autoCompleteAccompagnement.setThreshold(1);
         autoCompleteAccompagnement.setAdapter(adapterAccompagnements);
 
+        etQ2 = (EditText) view.findViewById(R.id.accompagmentPartageQuantiteEditText);
+
         adapterBoissons = new ArrayAdapter<>(view.getContext(),
                 android.R.layout.simple_dropdown_item_1line, listeBoissons);
         AutoCompleteTextView autoCompleteBoisson = view.findViewById(R.id.boissonPartageEditText);
         autoCompleteBoisson.setThreshold(1);
         autoCompleteBoisson.setAdapter(adapterBoissons);
+
+        etQ3 = (EditText) view.findViewById(R.id.boissonPartageQuantiteEditText);
         return view ;
     }
 
